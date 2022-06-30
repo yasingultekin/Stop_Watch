7c5ca8e307272d5366f7f9abb23a2848b1917517
yasingultekin <yasingultekin13@gmail.com>
2022-06-30 08:19:05 +0300

    js

--- app.js
+++ app.js
@@ -3,3 +3,19 @@ const second = document.querySelector(".second");
 const miliSecond = document.querySelector(".milisecond");
 const playBtn = document.querySelector(".play");
 const resetBtn = document.querySelector(".reset");
+
+let interval;
+
+playBtn.addEventListener("click", () => {
+  if (playBtn.innerText == "Play") {
+    playBtn.innerText = "Pause";
+    interval = setInterval(stopwatch, 10);
+  } else {
+    clearInterval(interval);
+    playBtn.innerText = "Play";
+  }
+});
+
+const stopwatch = () => {
+  miliSecond.innerText++;
+};
