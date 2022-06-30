328442d95f76777b68b9e45628292aa928781972
yasingultekin <yasingultekin13@gmail.com>
2022-06-30 08:16:53 +0300

    js

---
+++ app.js
@@ -0,0 +1,5 @@
+const minute = document.querySelector(".minute");
+const second = document.querySelector(".second");
+const miliSecond = document.querySelector(".milisecond");
+const playBtn = document.querySelector(".play");
+const resetBtn = document.querySelector(".reset");
